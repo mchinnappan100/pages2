e470744dbb5b3c97aa49f94a1b89cb5e688510cc
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2025-08-26 16:56:02 -0400

    greetings-added

---
+++ pmd/replace.sh
@@ -0,0 +1,2 @@
+curl -L "https://raw.githubusercontent.com/mchinnappan100/pages2/refs/heads/main/pmd/apexPmd.js" \
+  -o ~/.vscode/extensions/mohanchinnappan.apex-pmd-code-scanner-0.0.40/out/src/lib/apexPmd.js
